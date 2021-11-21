289a78b1910dade53d2e7f763e1bc97a004d7f2c
Raphael Boussidan <61468582+raphael2618@users.noreply.github.com>
2021-11-21 12:02:21 +0200

    Add files via upload
    akinator

---
+++ akinator.py
@@ -0,0 +1,23 @@
+import akinator
+
+aki = akinator.Akinator()
+
+q = aki.start_game('en')
+
+while aki.progression <= 90:
+    a = input(q + "\n\t")
+    if a == "b":
+        try:
+            q = aki.back()
+        except akinator.CantGoBackAnyFurther:
+            pass
+    else:
+        q = aki.answer(a)
+aki.win()
+
+correct = input(
+    f"It's {aki.first_guess['name']} ({aki.first_guess['description']})! Was I correct?\n{aki.first_guess['absolute_picture_path']}\n\t")
+if correct.lower() == "yes" or correct.lower() == "y":
+    print("Yay\n")
+else:
+    print("Oof\n")
